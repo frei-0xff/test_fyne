77a302971638da833b9798a07232701bf23434cc
frei-0xff <frei.0xff@gmail.com>
2022-06-17 19:12:00 +0300

    ЦЦЦ

--- .github/release.sh
+++ .github/release.sh
@@ -20,8 +20,7 @@ EXT='.exe'
 fi
 FILE_LIST="${PROJECT_NAME}${EXT}"
 
-# go install fyne.io/fyne/v2/cmd/fyne
-fyne package -os $GOOS -name $FILE_LIST
+fyne package -os $GOOS/$GOARCH -name $FILE_LIST
 
 FILE_LIST="${FILE_LIST} ${EXTRA_FILES}"
 
